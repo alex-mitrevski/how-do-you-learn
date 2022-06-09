8a8e7e61780c0459249cc490a133122f6537dc54
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2022-06-09 22:45:54 +0200

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='learner',
+      version='1.0.0',
+      description='A library containing implementations of various machine learning algorithms',
+      url='https://github.com/alex-mitrevski/how-do-you-learn',
+      author='Alex Mitrevski',
+      author_email='aleksandar.mitrevski@h-brs.de',
+      project_urls={
+          'Source': 'https://github.com/alex-mitrevski/how-do-you-learn'
+      })
