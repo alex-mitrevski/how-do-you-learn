8a15f54de8143cc03c52412c69157ea31f95c782
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2022-06-11 19:21:19 +0200

    [scripts/test/ppo+reinforce_test] Added an arg for a pretrained model

--- scripts/test/reinforce_test.py
+++ scripts/test/reinforce_test.py
@@ -16,6 +16,8 @@ if __name__ == '__main__':
                         help='Number of episodes per training iteration')
     parser.add_argument('-r', '--training_runs', type=int, required=True,
                         help='Number of times to repeat the training')
+    parser.add_argument('-pm', '--pretrained_model_path', type=str, default=None,
+                        help='Path to a pretrained model (default None)')
     parser.add_argument('-d', '--debug', action='store_true')
     args = parser.parse_args()
 
@@ -25,6 +27,7 @@ if __name__ == '__main__':
         print(f'Starting training run {i+1}')
         agent = ReinforceAgent(obs_len=env.observation_space.shape[0],
                                number_of_actions=env.action_space.n,
+                               pretrained_model_path=args.pretrained_model_path,
                                debug=args.debug)
         average_returns = agent.train(env=env,
                                       number_of_iterations=args.number_of_iterations,
